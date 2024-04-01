95be9cf3ad057ff972c765e1e2796b6789f7da1f
ritesh goswami <riteshg@witmates.com>
2024-04-01 11:39:25 +0530

    fix dir name for t-t-l

--- src/utilities/string.pipes.js
+++ src/utilities/string.pipes.js
@@ -1,18 +1,6 @@
 export default function stringPipe(str, type) {
    if (type === "t-t-l") {
-      if (str.includes("-")) {
-         let x = "";
-         str.split("-").forEach((letter, i) => {
-            if (i == 0) {
-               x += letter[0].toLowerCase() + letter.substring(1);
-            } else {
-               x += letter[0].toUpperCase() + letter.substring(1);
-            }
-         });
-         return x;
-      } else {
-         return str[0] + str.substring(1);
-      }
+      return str.toLowerCase();
    }
    if (type === "t-t-c") {
       if (str.includes("-")) {
